a166c6a0d678280fc819f5f630df36fc2ae5d405
Anjesh Singh <singhanjesh2005@gmail.com>
2025-06-29 03:21:58 +0530

    endpoints for routes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,19 @@
  const app = express();
 
  app.get("/api/notes", (req, res) => {
-    res.send("You have no notes my 1 mf!!!!!!!!");
+    res.status(200).send("you have 20 notes mf");
+ });
+
+ app.post("/api/notes", (req, res) => {
+    res.status(201).json({message:"note created successfully"});
+ });
+
+ app.put("/api/notes/:id", (req, res) => {
+    res.status(200).json({message:"note updated successfully"});
+ });
+
+ app.delete("/api/notes/:id", (req, res) => {
+    res.status(200).json({message:"note deleted successfully"});
  });
 
  app.listen(5001, () => {
