9921dd5262bc9f96ebe28d2bffc9ac83012efaaf
Anjesh Singh <singhanjesh2005@gmail.com>
2025-06-29 04:08:29 +0530

    optimised file and folder structure

---
+++ backend/src/routes/nodeRoutes.js
@@ -0,0 +1,30 @@
+import express from "express"
+import { createNote, deleteNote, getAllNotes, updateNote } from "../controllers/notesController.js";
+
+const router = express.Router();
+
+router.get("/", getAllNotes);
+
+router.post("/", createNote);
+
+ router.put("/:id", updateNote);
+
+ router.delete("/:id", deleteNote);
+
+export default router;
+
+//  app.get("/api/notes", (req, res) => {
+//     res.status(200).send("you have 20 notes mf");
+//  });
+
+//  app.post("/api/notes", (req, res) => {
+//     res.status(201).json({message:"note created successfully"});
+//  });
+
+//  app.put("/api/notes/:id", (req, res) => {
+//     res.status(200).json({message:"note updated successfully"});
+//  });
+
+//  app.delete("/api/notes/:id", (req, res) => {
+//     res.status(200).json({message:"note deleted successfully"});
+//  });
