0237e5871f41d8e9022e8ff0a40bee6b069741da
Anjesh Singh <singhanjesh2005@gmail.com>
2025-06-30 01:30:44 +0530

    add rate limiting using upstash and update routes

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -10,19 +10,3 @@ router.put("/:id", updateNote);
 router.delete("/:id", deleteNote);
 
 export default router;
-
-//  app.get("/api/notes", (req, res) => {
-//     res.status(200).send("you have 20 notes mf");
-//  });
-
-//  app.post("/api/notes", (req, res) => {
-//     res.status(201).json({message:"note created successfully"});
-//  });
-
-//  app.put("/api/notes/:id", (req, res) => {
-//     res.status(200).json({message:"note updated successfully"});
-//  });
-
-//  app.delete("/api/notes/:id", (req, res) => {
-//     res.status(200).json({message:"note deleted successfully"});
-//  });
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,20 +2,32 @@
  import notesRoutes from "./routes/notesRoutes.js";
  import {connectDB} from "./config/db.js";
  import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
  dotenv.config();
 
  const app = express();
  const PORT = process.env.PORT || 5001;
 
- connectDB();
 
  //middleware
- app.use(express.json()); 
+ app.use(express.json());  // helps parse JSON bodoes : req.body
+
+ app.use(rateLimiter);
+
+//  simple custom middleware to log request method and URL
+//   app.use((req,res,next) => {
+//       console.log(`Req method is ${req.method} and URL is ${req.url}`);
+//       next();
+//  });
 
  app.use("/api/notes", notesRoutes);
 
- app.listen(5001, () => {
-    console.log("Server started on PORT: ",PORT);
+ 
+ connectDB().then(() => {
+      app.listen(PORT, () => {
+      console.log("Server started on PORT: ",PORT);
+   });
  });
+ 
 
